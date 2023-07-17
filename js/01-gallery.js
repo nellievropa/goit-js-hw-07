8b6b8b605e36265ddbbe0dd08dc094c1a59964b2
nellievropa <nellievropa23@gmail.com>
2023-07-17 15:01:15 +0300

    01 without ESC

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,18 +5,19 @@ console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const itemMarkup = createGalleryMarkup(galleryItems);
-
+const buttonClick = document.querySelector('.gallery');
 
 galleryContainer.insertAdjacentHTML('beforeend', itemMarkup);
-galleryContainer.addEventListener('click', onClick);
-// galleryContainer.addEventListener('click', onGalleryContainer);
+galleryContainer.addEventListener('click', onImageClick);
+buttonClick.addEventListener("keydown",onEscKeyPress);
+
 
 function createGalleryMarkup(galleryItems) {
     return galleryItems
     .map(({preview, original})=> {
         return `
         <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -34,69 +35,66 @@ function createGalleryMarkup(galleryItems) {
 
 console.log(createGalleryMarkup(galleryItems));
 
-// [...galleryContainer.children].forEach(item => item.addEventListener('click', onClick))
 
-function  onClick(event) {
-  // знайти Url
+function  onImageClick(event) {
+console.log(event.target.classList)
+event.preventDefault();
   if(!event.target.classList.contains("gallery__image")) {
     return;
   }
   const originalUrl = event.target.dataset.source;
-  console.log(originalUrl);
-}
-
-
-
-// 
-// const currentActiveImg =  document.querySelector('.gallery')
-// currentActiveImg.addEventListener('click', onGalleryContainer);
-// function onGalleryContainer(event) {
-//   if(event.target.nodeName !== 'IMG') {
-//     return;
-//   }
-
-//   const currentActiveBtn = document.querySelector('.gallery__image--active')
-// }
-
-// const instance = document.querySelector('.gallery__image').onclick = () => {
-
-// 	basicLightbox.create(`
-//      <div class="modal">
-// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
-//     </div>
-// 	`).show()
-
-// }
-
-// console.log(instance);
-
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-     
-    </div>
+  const addModal = event.target.classList.add('--show-modal');
+  console.log(addModal);
+  // console.log(originalUrl);
+  const instance = basicLightbox.create(`
+  <img src="${originalUrl}" width="1280" height="600">
 `)
 
 instance.show()
+onEscKeyPress()
+}
 
 
-// function onGalleryContainer(event) {
+function onCloseModal() {
+  // і відписуємось від  window.addEventListener('keydown', onEscKeyPress); перед закриттям модалки
+  window.removeEventListener('keydown', onEscKeyPress);
+  document.body.classList.remove('show-modal'); 
+}
+
+function onEscKeyPress(evt) {
+ 
+  console.log(evt.code);
+
+  // так працює і закривається при натисканні любої клавіші!
+  // томe треба додати умову с Code:
+  // можна ще змінну додати
+  const ESC_KEY_CODE = "Escape";
+  if(evt.target.code === ESC_KEY_CODE) {
+      onCloseModal();
+  }
+  
+  };
+
+
+
+
+
+
+// function onClick(event) {
 // // console.log(event.target);
 
-// const isImageEl = event.target.classList.contains('gallery__image');
-
+// const isImageEl = event.target
 // console.log(isImageEl);
-// const linkEl = document.querySelector('.gallery__link').value;
-// console.log(linkEl);
+// // .contains('gallery__image');
+// // const linkEl = document.querySelector('.gallery__link');
+// // console.log(linkEl);
 // if(!isImageEl) {
 //     // event.stopImmediatePropagation(linkEl);
 //     event.preventDefault();
 // }
 
-// document.body.style.backgroundColor = teal;
+// isImageEl.classList.add("--active");
+// console.log(isImageEl.classList)
 
 
 // }
